6cad3f31f2a686e8bc7d97005267c4f2c5355afd
kunyuli <kunyuli16@gmail.com>
2021-09-28 20:40:17 +0800

    Initial Commit

---
+++ Classic07/Classic07App.swift
@@ -0,0 +1,17 @@
+//
+//  Classic07App.swift
+//  Classic07
+//
+//  Created by 暨大附中4 on 2021/9/28.
+//
+
+import SwiftUI
+
+@main
+struct Classic07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
